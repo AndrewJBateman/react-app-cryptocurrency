d3cf3332a676b20a4739042ea068bdd05c1e6d6b
AndrewJBateman <gomezbateman@yahoo.com>
2022-03-22 12:14:54 +0100

    chore: code linting

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 /*ES6 modules*/
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import NumberFormat from 'react-number-format';
+import React, {Component} from "react";
+import "./App.css";
+import axios from "axios";
+import NumberFormat from "react-number-format";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
-		// holds response from API
+    // holds response from API
     this.state = {
-      cryptos: []
+      cryptos: [],
     };
   }
 
-	/* 
+  /* 
 	function will be carried out once app loads.
 	Axios is promise-based so 'then' function used.
 	Cryptocompare returns price of bitcoins compared to the US $. 
 	setState puts changes in the component state in a queue and requests React to re-render using the updated state.
 	*/
   componentDidMount() {
-		const fsymsList = 'BTC,XRP,BCH,ETH,ZEC,EOS,XMR,ETC,LTC,DASH,QTUM,NEO,XLM,TRX,ADA,BTS,USDT,XUC,PAX,IOT'
-		axios
-			.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + fsymsList + '&tsyms=USD,EUR')
-      .then(res => {
-        const cryptos = res.data;
-        console.log(cryptos);
-        this.setState({cryptos});
-      });
+    const fsymsList = "BTC,XRP,BCH,ETH,ZEC,EOS,XMR,ETC,LTC,DASH,QTUM,NEO,XLM,TRX,ADA,BTS,USDT,XUC,PAX,IOT";
+    axios.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + fsymsList + "&tsyms=USD,EUR").then((res) => {
+      const cryptos = res.data;
+      console.log(cryptos);
+      this.setState({cryptos});
+    });
   }
 
   render() {
     return (
       <div className="App">
-				{Object.keys(this.state.cryptos).map((key) => (
-					<div id="crypto-container" key={key}>
-						<span className="left">{key}</span>
-						<span className="right"><NumberFormat 
-							value={this.state.cryptos[key].USD} 
-							displayType={'text'}
-							decimalprecision={2} 
-							thousandSeparator={true} 
-							prefix={'$'} />
-						</span>
-					</div>
-				))}
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-container" key={key}>
+            <span className="left">{key}</span>
+            <span className="right">
+              <NumberFormat
+                value={this.state.cryptos[key].USD}
+                displayType={"text"}
+                decimalprecision={2}
+                thousandSeparator={true}
+                prefix={"$"}
+              />
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
